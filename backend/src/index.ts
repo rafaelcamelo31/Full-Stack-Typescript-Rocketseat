ff28934b6f5fd7e16962032e36445cc160094ad0
Rafael <rafaelcamelo31@gmail.com>
2020-11-05 18:18:43 -0400

    dev:server config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send('Hello World');
+    return res.send('Hello World 2');
 });
 
 app.listen(5000);
